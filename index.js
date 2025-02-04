57571dd5f0645e4ddc9eb1c6bf17756658928ff7
Richard Chalk <barmybrit@gmail.com>
2025-02-04 21:38:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Show correct Exercise according to navbar choice!
+function showExercise(exerciseId) {
+  document.querySelectorAll(".container").forEach((container) => {
+    container.classList.remove("active");
+  });
+  document.getElementById(exerciseId).classList.add("active");
+}
+
+// Create Circle
+document.getElementById("circleButton").addEventListener("click", function () {
+  const shape = document.getElementById("shape");
+  shape.style.borderRadius = "50%";
+  shape.style.backgroundColor = "blue";
+});
+
+// Create square
+document.getElementById("squareButton").addEventListener("click", function () {
+  const shape = document.getElementById("shape");
+  shape.style.borderRadius = "0";
+  shape.style.backgroundColor = "red";
+});
+
+// Move square when hovering mouse over it
+document
+  .getElementById("movingSquare")
+  .addEventListener("mouseover", function () {
+    const square = document.getElementById("movingSquare");
+    const maxX = window.innerWidth - 100;
+    const maxY = window.innerHeight - 100;
+    // Geenrates a whole number between 0-100
+    square.style.left = Math.floor(Math.random() * maxX) + "px";
+    square.style.top = Math.floor(Math.random() * maxY) + "px";
+  });
